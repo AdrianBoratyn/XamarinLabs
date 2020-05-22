79f7cd68b9fe18d78fda0a8857e03014c02b9bd7
Adrian Boratyn <adrian.boratyn@gmail.com>
2020-05-22 23:06:54 +0200

    Lab4

---
+++ Laboratorium4/Laboratorium4/Laboratorium4/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Laboratorium4.Models;
+
+namespace Laboratorium4.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
---
+++ Laboratorium4/Laboratorium4/Laboratorium4/Views/zad4.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Laboratorium4.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class zad4 : ContentPage
+    {
+        public zad4()
+        {
+            InitializeComponent();
+        }
+        private void Switch_Toggled(Object sender, ToggledEventArgs e)
+        {
+            if (e.Value)
+            {
+                sView.Orientation = ScrollOrientation.Horizontal;
+                sLayout.Orientation = StackOrientation.Horizontal;
+                l1.Text = "Zmień orientacje, obecna: Horyzontalna";
+            }
+            else
+            {
+                sView.Orientation = ScrollOrientation.Vertical;
+                sLayout.Orientation = StackOrientation.Vertical;
+                l1.Text = "Zmień orientacje, obecna: Vertical";
+            }
+        }
+    }
+}
